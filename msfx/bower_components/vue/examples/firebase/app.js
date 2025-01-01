f2eb256c07828b385b29d1c0dd4120353a4ceebf
ArmsHuang <armsdream@gmail.com>
2025-01-01 21:27:05 +0800

    Update app.js

--- msfx/bower_components/vue/examples/firebase/app.js
+++ msfx/bower_components/vue/examples/firebase/app.js
@@ -2,7 +2,6 @@ var emailRE = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@(
 
 // Setup Firebase
 var config = {
-  apiKey: "AIzaSyAi_yuJciPXLFr_PYPeU3eTvtXf8jbJ8zw",
   authDomain: "vue-demo-537e6.firebaseapp.com",
   databaseURL: "https://vue-demo-537e6.firebaseio.com"
 }
